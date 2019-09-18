94e74d3876923cbfc7cb612abe9fe767ccf7e5e7
Brandon Mowat <me@brandonmowat.com>
2019-09-18 15:18:00 -0400

    init

---
+++ src/clojureserver/core.clj
@@ -0,0 +1,20 @@
+(ns clojureserver.core
+  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [org.httpkit.server :refer [run-server]]
+            [clojure.string :as str])
+  (:gen-class))
+
+(defroutes clojureserver
+  (GET "/" [] "Hello World"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (prn 'STARTING-SERVER "http://localhost:5000")
+  (def stop-server
+    (-> (wrap-reload #'clojureserver)
+        (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+        (run-server {:port 5000}))))
